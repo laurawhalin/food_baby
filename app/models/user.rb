4d50dca23de758f6a6268e662be8a215ee979f40
Laura Whalin <laurawhalin@gmail.com>
2015-03-05 11:35:33 -0700

    obtained a larger image from facebook, and added login/logout logic to header. Next step is to get a collapsing navbar to hide the navbar options that don't fit.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,7 @@ class User < ActiveRecord::Base
     user = User.find_or_create_by(uid: auth.uid)
 
     user.email = auth.info.email
-    user.name = auth.user_name
-    require 'pry' ; binding.pry
+    user.name = self.user_name(auth)
     user.image_url = auth.info.image
     user.token = auth.credentials.token
     user.save
@@ -13,7 +12,7 @@ class User < ActiveRecord::Base
     user
   end
 
-  def user_name
-    auth.info.first_name + auth.info.last_name[0] + "."
+  def self.user_name(auth)
+    "#{auth.info.first_name} #{auth.info.last_name[0]}."
   end
 end
