2792c5aab95d467a33790409d49a327a7fdb8fea
Laura Whalin <laurawhalin@gmail.com>
2015-03-09 08:16:15 -0600

    populated results map with seed data within a constrained locale.

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,4 +1,12 @@
 class RestaurantsController < ApplicationController
   def show
   end
+
+  def index
+    @restaurants = Restaurant.all
+    @hash = Gmaps4rails.build_markers(@restaurants) do |restaurant, marker|
+      marker.lat restaurant.latitude
+      marker.lng restaurant.longitude
+    end
+  end
 end
