10794ee5ef7063916966abf65ef9ddce66ce35aa
Laura Whalin <laurawhalin@gmail.com>
2015-03-11 14:58:02 -0600

    removed old gmaps4rails code

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,47 +1,8 @@
-// // navigator.geolocation.getCurrentPosition(function (position ) {console.log(position)})
-// var watchID = navigator.geolocation.watchPosition(function(position) {
-//   console.log(position.coords.latitude, position.coords.longitude);
-//   socket.emit('changeLocation', {
-//     lat: position.coords.latitude,
-//     long: position.coords.longitude
-//   });
-// });
-//
-//
-// var geocoder;
-// var map;
-// function initialize() {
-//   geocoder = new google.maps.Geocoder();
-//   var latlng = new google.maps.LatLng(watchID);
-//   var mapOptions = {
-//     zoom: 8,
-//     center: latlng
-//   }
-//   map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-// }
-//
-// function codeAddress() {
-//   var address = document.getElementById('address').value;
-//   geocoder.geocode( { 'address': address}, function(results, status) {
-//     if (status == google.maps.GeocoderStatus.OK) {
-//       map.setCenter(results[0].geometry.location);
-//       var marker = new google.maps.Marker({
-//         map: map,
-//         position: results[0].geometry.location
-//       });
-//     } else {
-//       alert('Geocode was not successful for the following reason: ' + status);
-//     }
-//   });
-// }
-//
-// google.maps.event.addDomListener(window, 'load', initialize);
-
 var map;
 
 function initialize() {
   var mapOptions = {
-    zoom: 6
+    zoom: 10
   };
   map = new google.maps.Map(document.getElementById('map-canvas'),
   mapOptions);
@@ -77,7 +38,7 @@ function initialize() {
 
     var options = {
       map: map,
-      position: new google.maps.LatLng(60, 105),
+      position: new google.maps.LatLng(39.7391500, -104.9847000),
       content: content
     };
 
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,17 +1,9 @@
 class RestaurantsController < ApplicationController
   def show
     @restaurant = Restaurant.find(params[:id])
-    # @hash = Gmaps4rails.build_markers(@restaurant) do |restaurant, marker|
-    #   marker.lat restaurant.latitude
-    #   marker.lng restaurant.longitude
-    # end
   end
 
   def index
     @restaurants = Restaurant.kid_type(params[:format])
-    # @hash = Gmaps4rails.build_markers(@restaurants) do |restaurant, marker|
-    #   marker.lat restaurant.latitude
-    #   marker.lng restaurant.longitude
-    # end
   end
 end
